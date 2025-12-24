e17b8147ea9c2c3d070adf4ae6b0f94d134da7dc
Tanmoy Das <tanmoyofc02@gmail.com>
2025-12-23 19:19:06 -0600

    Add files via upload

---
+++ Maximum of three numbers.py
@@ -0,0 +1,8 @@
+print("what are the numbers?")
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+num3 = float(input("Enter third number: "))
+
+maximum = max(num1, num2, num3)
+
+print("The maximum is:", maximum)
---
+++ Minimum of there numbers.py
@@ -0,0 +1,8 @@
+print("what are your numbers?")
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+num3 = float(input("Enter third number: "))
+
+minimum = min(num1, num2, num3)
+
+print("The minimum is:", minimum)
